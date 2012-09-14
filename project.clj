bf636b98339f2bfc75e196b0249d41cf7349310f
Anthony Grimes <i@raynes.me>
2012-09-14 16:16:15 -0500

    Update clojail to get transform fix.

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clojail "1.0.1"]])
+                 [clojail "1.0.2"]])
