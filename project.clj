6d066a4fd1784b283a723d08a81079c42914211f
Anthony Grimes <i@raynes.me>
2012-09-14 20:18:24 -0500

    Update clojail and inc version.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject findfn "0.1.2"
+(defproject findfn "0.1.3"
   :url "https://github.com/Raynes/findfn"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clojail "1.0.2"]]
+                 [clojail "1.0.3"]]
   :jvm-opts ["-Djava.security.policy=example.policy"])
